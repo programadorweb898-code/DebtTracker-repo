15f50a01c53100513e9ab23db34526ef68c9b93a
Luis Gomez <gomito724@gmail.com>
2025-12-12 22:32:51 +0000

    Estás variables de entorno, también las tengo que tener en mí archivo .e

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,7 @@
+import 'dotenv/config';
+import next from 'next';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
