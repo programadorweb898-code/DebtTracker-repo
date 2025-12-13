09321f06d8e17ea1e35cae706b642186105a486a
luis alberto <programadorweb898@gmail.com>
2025-12-13 11:53:44 -0300

    archivos actualizados

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 import 'dotenv/config';
-import next from 'next';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone', // Importante para Render - optimiza el build
+  eslint: {
+    ignoreDuringBuilds: true, // Evita que errores de ESLint detengan el build
+  },
+  typescript: {
+    ignoreBuildErrors: false, // Mantén esto en false para detectar errores de TypeScript
+  },
+};
 
 export default nextConfig;
